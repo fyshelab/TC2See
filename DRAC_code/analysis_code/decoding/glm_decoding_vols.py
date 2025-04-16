22fd7b159ee125c85c2356a4f57dfc3c7f8d1d09
James McKinnon <jam10@ualberta.ca>
2025-04-16 01:31:28 -0700

    fixed bug with normalized values not being used

--- DRAC_code/analysis_code/decoding/glm_decoding_vols.py
+++ DRAC_code/analysis_code/decoding/glm_decoding_vols.py
@@ -17,38 +17,38 @@ from noise_ceiling import (
     compute_nc,
 )
 
-# dir2 = os.path.abspath('../..')
-# dir1 = os.path.dirname(dir2)
-# if not dir1 in sys.path: 
-#     sys.path.append(dir1)
-
 dataset_root = os.path.expanduser('~/projects/def-afyshe-ab/TC2See')
 james_root = Path(__file__).parent.parent.parent.parent.parent
 tc2see_version = 3
 
 accuracies = {}
 
-subjs = ['05']
+subjs = ['05', '06', '07', '08', '09', '10', '11', '12', '14', '15', '16', '17', '18', '19', '20',
+         '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', 
+         '36', '37', '38', '39', '40']
 
 for subj in tqdm(subjs):
-    tr = 2 # 1.97
-    subject_no = subj 
-    subject = f'sub-{subject_no}'
-    
+    try:
+        tr = 2 # 1.97
+        subject_no = subj 
+        subject = f'sub-{subject_no}'
 
-    # bold, stimulus_ids, mask, affine = load_data(
-    #     f'{dataset_root}/tc2see-betas_GLMsingle_T1w.hdf5', ################### just put this in ###############
-    #     # f'../data/processed/hdf5s/tc2see-v{tc2see_version}-bold.hdf5', 
-    #     subject,
-    #     tr_offset=6 / tr,
-    #     run_normalize='linear_trend',
-    #     interpolation=False,
-    # )
+        model_name = 'ViT-B=32'
+        embedding_name = 'embedding' 
 
+        file_path = f'{dataset_root}/tc2see-betas_GLMsingle_T1w.hdf5' 
+        h5_file = h5py.File(file_path, 'r')
 
-    model_name = 'ViT-B=32'
-    embedding_name = 'embedding' 
+        # Extract each key as a variable
+        betas = h5_file[f'{subject}/betas'][()]
+        betas_mean = h5_file[f'{subject}/betas_mean'][()]
+        betas_std = h5_file[f'{subject}/betas_std'][()]
+        noise_ceiling = h5_file[f'{subject}/noise_ceiling'][()]
+        stimulus_ids = h5_file[f'{subject}/stimulus_ids'][()]
+        betas_normalized = (betas - betas_mean[:, None]) / betas_std[:, None]
+        fmri_mask = h5_file[f'{subject}/fmri_mask'][()]
 
+        h5_file.close()
 
     file_path = f'{dataset_root}/tc2see-betas_GLMsingle_T1w.hdf5' 
     h5_file = h5py.File(file_path, 'r')
@@ -114,33 +114,77 @@ for subj in tqdm(subjs):
         X_nan_test = np.isnan(X_test) # Checks if any not a number values in x and sets those to zero
         X_test[X_nan_test] = 0.
 
+        # load the clip embeddings
         with h5py.File(f'../../data/{model_name}-features.hdf5', 'r') as f:
             stimulus = f[embedding_name][:]
-        Y_train = stimulus[stimulus_ids_train] 
-        Y_test = stimulus[stimulus_ids_test]
+        Y = stimulus[stimulus_ids] # get the stimulus representations to decode
 
-        if permutation_test:
-            ids = np.arange(Y_train.shape[0]) 
-            np.random.shuffle(ids)
-            Y_train = Y_train[ids]
 
-        model = FracRidgeRegressorCV()
-        model.fit(X_train, Y_train) 
-        Y_test_pred = model.predict(X_test) # Y_test and Y_test_pred are n x 512 matrics (n is the number of birds).
+        subject = f'sub-{subject_no}'
+        results = dict
+        permutation_test = False
+        num_runs = 6
 
-        distances = cosine_distance( 
-            torch.from_numpy(Y_test[None]).float(), 
-            torch.from_numpy(Y_test_pred[:, None]).float()
-        ) 
+        accuracies_list = []
+            
+        # Cross validation. Use every id as test data once.
+        for test_run_id in tqdm(range(num_runs)):
+            training_run_ids = list(range(num_runs))
+            training_run_ids.remove(test_run_id) # Remove the test data id 
 
-        print(f"Distances shape: {distances.shape}")
-        stimulus_ids_flat = stimulus_ids.reshape(-1)
-        accuracy = round(two_versus_two(distances, stimulus_ids=stimulus_ids_flat).item() * 100, 2) 
-        accuracies_list.append(accuracy)
+            bold_train = betas_normalized[training_run_ids, :, :]      # Shape: (5, 75, 200258)
+            bold_train = bold_train.reshape(-1, bold_train.shape[-1])  # Shape: (375, 200258)
 
-    accuracy_avg = np.mean(accuracies_list)
-    accuracies[subj] = accuracy_avg
-    print(f"Subject {subj} accuracy: ", accuracy_avg)
+            stimulus_ids_train = stimulus_ids[training_run_ids, :] # Shape: (5, 75)
+            stimulus_ids_train = stimulus_ids_train.reshape(-1)    # Shape: (375,)
+
+            bold_test = betas_normalized[test_run_id:test_run_id+1, :, :]         # Shape: (1, 75, 200258)
+            bold_test = bold_test.reshape(bold_test.shape[1], bold_test.shape[2]) # Shape: (75, 200258)
+            stimulus_ids_test = stimulus_ids[test_run_id:test_run_id+1, :].reshape(-1) # Shape: (1, 75)
+            
+            ncsnr = compute_ncsnr(bold_train, stimulus_ids_train) # Compute noise ceiling noise ratio
+            nc = compute_nc(ncsnr, num_averages=1)
+
+            nc_vc = nc.copy() 
+            argsort_ids = np.argsort(-nc_vc) # Default ascending, make descending 
+            argsort_ids = argsort_ids[:256] 
+
+            X_train = bold_train[:, argsort_ids]    
+            X_nan_train = np.isnan(X_train) # Checks if any not a number values in x and sets those to zero
+            X_train[X_nan_train] = 0.
+
+            X_test = bold_test[:, argsort_ids]
+            X_nan_test = np.isnan(X_test) # Checks if any not a number values in x and sets those to zero
+            X_test[X_nan_test] = 0.
+
+            with h5py.File(f'../../data/{model_name}-features.hdf5', 'r') as f:
+                stimulus = f[embedding_name][:]
+            Y_train = stimulus[stimulus_ids_train] 
+            Y_test = stimulus[stimulus_ids_test]
+
+            if permutation_test:
+                ids = np.arange(Y_train.shape[0]) 
+                np.random.shuffle(ids)
+                Y_train = Y_train[ids]
+
+            model = FracRidgeRegressorCV()
+            model.fit(X_train, Y_train) 
+            Y_test_pred = model.predict(X_test) 
+
+            distances = cosine_distance( 
+                torch.from_numpy(Y_test[None]).float(), 
+                torch.from_numpy(Y_test_pred[:, None]).float()
+            ) 
+
+            stimulus_ids_flat = stimulus_ids.reshape(-1)
+            accuracy = round(two_versus_two(distances, stimulus_ids=stimulus_ids_flat).item() * 100, 2) 
+            accuracies_list.append(accuracy)
+
+        accuracy_avg = np.mean(accuracies_list)
+        accuracies[subj] = accuracy_avg
+        print(f"Subject {subj} accuracy: ", accuracy_avg)
+    except Exception as e:
+        print(f"Error processing subject {subj}: {e}")
 
 print(accuracies)
 
